01e2f57ab0374f0dc016113f99f68351a675c020
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-05-04 15:44:57 +0530

    form

---
+++ src/components/Form.js
@@ -0,0 +1,63 @@
+// Form.js
+import React, { useState } from 'react';
+import './../styles/App.css';
+
+const Form = () => {
+  const [fields, setFields] = useState([{ name: '', age: '' }]);
+
+  const handleInputChange = (index, event) => {
+    const { name, value } = event.target;
+    const newFields = [...fields];
+    newFields[index][name] = value;
+    setFields(newFields);
+  };
+
+  const handleAddField = () => {
+    setFields([...fields, { name: '', age: '' }]);
+  };
+
+  const handleRemoveField = (index) => {
+    const newFields = [...fields];
+    newFields.splice(index, 1);
+    setFields(newFields);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Form Data:', fields);
+  };
+
+  return (
+    <form   className='form' onSubmit={handleSubmit}>
+      {fields.map((field, index) => (
+        <div key={index}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={field.name}
+            onChange={(e) => handleInputChange(index, e)}
+          />
+          <input
+            type="text"
+            name="age"
+            placeholder="Age"
+            value={field.age}
+            onChange={(e) => handleInputChange(index, e)}
+          />
+          <button type="button" onClick={() => handleRemoveField(index)}>
+            Remove
+          </button>
+        </div>
+      ))}
+      <button className='add' type="button" onClick={handleAddField}>
+        Add More ...
+      </button>
+      <button className='sub' type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
+
+
